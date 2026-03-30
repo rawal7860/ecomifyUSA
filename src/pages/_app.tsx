b17181edab882159c388a840288cab49b9b62882
Shahzad Khaliq <rawal16@gmail.com>
2026-03-30 22:48:53 +0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <WhatsAppButton />
       <Toaster />
     </>
   );
